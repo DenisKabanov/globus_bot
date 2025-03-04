8ecce26c392483deafb2ffc5f2034d3b1e3500b4
ElenaRodionova <day06112001@yandex.ru>
2025-03-04 20:41:54 +0300

    добавление команд

--- src/bot.py
+++ src/bot.py
@@ -2,29 +2,65 @@
 # файл для запуска телеграм бота
 #===============================================================================
 
+import json
 import os # для переменных окружения (токена бота)
 import random # для случайного выбора
 import sqlite3 # для работы с базами данных
+import numpy as np
 from dotenv import load_dotenv # для загрузки переменных окружения
-from telegram import Update
+from telebot import TeleBot
+from telegram import Update, KeyboardButton
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram.ext import MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup
-import logging # для логирования
+#import logging # для логирования
+
 
 load_dotenv() # загрузка переменных окружения
 token = os.getenv('TOKEN') # берём токен бота из переменных окружения
+data_path = os.getenv('DATA_PATH')
+bot = TeleBot(token=token) 
 
-logging.basicConfig(format='%(asctime)s – %(name)s – %(levelname)s – %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+data = {}
+
+for country_name in os.listdir(data_path):
+    
+    data[country_name] = {}
+    data[country_name]["flag"] = f"{data_path}/{country_name}/flag.png"
+    data[country_name]["map"] = f"{data_path}/{country_name}/map.png"
+
+    data[country_name]["description"] = {}
+    
+    with open(f"{data_path}/{country_name}/description.txt", "r", encoding="utf-8") as f:
+        
+        for hint in ["Природа", "Достопримечательности", "Культура", "Язык", "Исторический факт", "Города"]:
+            
+            data[country_name]["description"][hint] = f.readline()[len(hint) + 2:]
+            f.readline()
+        
+        data[country_name]["description"]["Общее описание"] = f.readline(-1)    
 
 
 # Функция-обработчик команды /start
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я ваш бот.')
-    keyboard = [['/start', '/help'], ['/repeat']]
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите команду:', reply_markup=reply_markup)
+    
+    user_id = update.message.chat.id
+    user_name = update.message.chat.first_name
+    
+    button1 = KeyboardButton("/start")
+    button2 = KeyboardButton("/help")
+    button3 = KeyboardButton("загадай")
+    button4 = KeyboardButton("расскажи о")
+
+    kb_commands = ReplyKeyboardMarkup(
+        keyboard=[
+            [button1, button2],
+            [button3, button4]
+        ],
+        resize_keyboard=True  # Optional: Resizes the keyboard to fit the screen
+    )
+
+    update.message.reply_text(f"Привет, {user_name}! Начнем викторину? \nВыберите команду:", reply_markup=kb_commands)
 
 
 def help(update: Update, context: CallbackContext):
@@ -38,10 +74,34 @@ def help(update: Update, context: CallbackContext):
 
 def repeat(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
+    
+def send_flag(update: Update, context: CallbackContext) -> None:
+    
+    country_number = random.choices(np.arange(len(data)), k=1)[0]
+    
+    country_name = list(data.keys())[country_number]
+    
+    flag_path = data[country_name]['flag']
+    
+    chat_id = update.message.chat.id
+    
+    context.bot.send_photo(chat_id=chat_id, photo=open(flag_path, 'rb'))
+    
+def tell_about(update: Update, context: CallbackContext) -> None:
+    
+    print(update)
+    print(context)
+    #country_name = list(data.keys())[country_number]
+    
+    #flag_path = data[country_name]['flag']
+    
+    #chat_id = update.message.chat.id
+    
+    #context.bot.send_photo(chat_id=chat_id, photo=open(flag_path, 'rb'))
 
 
-def error(update: Update, context: CallbackContext) -> None:
-    logger.warning(f'Update {update} caused error {context.error}')
+#def error(update: Update, context: CallbackContext) -> None:
+    #logger.warning(f'Update {update} caused error {context.error}')
 
 def init_db():
     conn = sqlite3.connect('bot.db')
@@ -66,8 +126,11 @@ def main():
     # Регистрация обработчика команд
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, repeat))
-    dispatcher.add_error_handler(error)
+    #dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, repeat))
+    dispatcher.add_handler(MessageHandler(Filters.regex(r"загадай"), send_flag))
+    dispatcher.add_handler(MessageHandler(Filters.regex(r"расскажи о"), tell_about))  
+    
+    #dispatcher.add_error_handler(error)
 
     updater.start_polling() # Запуск бота
 
